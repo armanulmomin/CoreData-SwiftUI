dd057c6622730f35ce670d2e4edf843aafc66453
Armanul Momin <armanul.momin@gmail.com>
2025-07-29 22:27:16 +1000

    first commit

---
+++ CoreData Practice/CoreData_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_PracticeApp.swift
+//  CoreData Practice
+//
+//  Created by Arman on 29/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
